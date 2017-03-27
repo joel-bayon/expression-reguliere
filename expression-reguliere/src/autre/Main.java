6d9b33c572332a913235679771764fa1c344418a
joel <joel@192.168.0.10>
2017-03-27 11:50:13 +0100

    first commit

---
+++ expression-reguliere/src/autre/Main.java
@@ -0,0 +1,16 @@
+package autre;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String adresse = "1831 Beverly Place #9D";
+		String pattern = "([0-9a-zA-Z ,\\.])*";
+		
+		System.out.println(adresse.matches(pattern));
+		System.out.println(Thread.currentThread().getId());
+
+	}
+
+}
